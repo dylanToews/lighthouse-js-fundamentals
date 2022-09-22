a20103960c05d0a3a4f57e5b87b7a985315280ff
Dylan Toews <dylan.toews@yahoo.ca>
2022-09-22 15:23:26 -0600

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,6 @@
-const raining = true;
-const cold = false;
+const isCitizen = true;
+const age = 26;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
+if (age > 18 && isCitizen){
+    console.log("You can vote baby");
 }
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
-
